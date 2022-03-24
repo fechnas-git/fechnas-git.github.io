0cb4e2086cecd968ddecf3e9ddc6433c6d169710
fechnas-git <fechnas@gmail.com>
2022-03-24 13:52:30 +0100

    Add Content

---
+++ assets/js/stickyheader.js
@@ -0,0 +1,15 @@
+window.onscroll = function () {
+  stickHeader();
+};
+
+var header = document.getElementById("Header");
+
+var sticky = header.offsetTop;
+
+function stickHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
