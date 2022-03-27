5b8fbc287aa3bea34e248c2ed65d6c33596a3bc2
fechnas-git <fechnas@gmail.com>
2022-03-27 14:23:25 +0200

    Add Content

--- assets/js/codeformatter.js
+++ assets/js/codeformatter.js
@@ -631,13 +631,14 @@ Prism.languages.gherkin = {
 };
 Prism.languages.csharp = Prism.languages.extend("clike", {
   keyword:
-    /\b(abstract|as|base|bool|break|byte|case|catch|char|checked|class|const|continue|decimal|default|delegate|do|double|else|enum|event|explicit|extern|false|finally|fixed|float|for|foreach|goto|if|implicit|in|int|interface|internal|is|lock|long|namespace|new|null|object|operator|out|override|params|private|protected|public|readonly|ref|return|sbyte|sealed|short|sizeof|stackalloc|static|string|struct|switch|this|throw|true|try|typeof|uint|ulong|unchecked|unsafe|ushort|using|virtual|void|volatile|while|add|alias|ascending|async|await|descending|dynamic|from|get|global|group|into|join|let|orderby|partial|remove|select|set|value|var|where|yield)\b/g,
+    /\b(1f|4f|abstract|as|base|bool|break|byte|case|catch|char|checked|class|const|continue|decimal|default|delegate|do|double|else|enum|event|explicit|extern|false|finally|fixed|float|for|foreach|goto|if|implicit|in|int|interface|internal|is|lock|long|namespace|new|null|object|operator|out|override|params|private|protected|public|readonly|ref|return|sbyte|sealed|short|sizeof|stackalloc|static|string|struct|switch|this|throw|true|try|typeof|uint|ulong|unchecked|unsafe|ushort|using|virtual|void|volatile|while|add|alias|ascending|async|await|descending|dynamic|from|get|global|group|into|join|let|orderby|partial|remove|select|set|value|var|where|yield)\b/g,
   string: /@?("|')(\\?.)*?\1/g,
   preprocessor: /^\s*#.*/gm,
   number: /\b-?(0x)?\d*\.?\d+\b/g,
-  unityvar: /\b(Vector3|LayerMask|Transform|Collider2D|Physics2D)\b/g,
+  unityvar:
+    /\b(Vector3|LayerMask|Transform|Collider2D|Physics2D|Enemy|Color)\b/g,
   literal:
-    /\b(i|directionArray|Length|collidableObjects|possibleMoveTiles|col|direction|layer|character|Characters|position)\b/g,
+    /\b(i|directionArray|Length|collidableObjects|possibleMoveTiles|col|direction|layer|character|Characters|position|playerMoves|enemiesInRange|enemy|Count|endTurnButton|interactable)\b/g,
 });
 Prism.languages.go = Prism.languages.extend("clike", {
   keyword:
